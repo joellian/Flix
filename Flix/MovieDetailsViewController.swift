71a80ea6d928f06c6d78635e5108f4db0d08bb98
Joel <Lianb1@nku.edu>
2022-09-24 00:14:05 -0400

    finishing staging

--- Flix/MovieDetailsViewController.swift
+++ Flix/MovieDetailsViewController.swift
@@ -6,13 +6,47 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MovieDetailsViewController: UIViewController {
+    
+    @IBOutlet weak var backdropView: UIImageView!
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    
+    
+    var movie: [String:Any]!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        
+    
+        titleLabel.text = movie["title"] as? String
+        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let baseURL = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterURl = URL(string: baseURL + posterPath)!
+        
+        posterView.af.setImage(withURL: posterURl)
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropURl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)!
+        
+        backdropView.af.setImage(withURL: backdropURl)
+        
+        
+    
     }
     
 
